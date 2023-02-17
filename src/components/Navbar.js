b038b78fde4ed810b2eca7b01ff6b25f572742cf
Ashish6745 <ashupawar9917@gmail.com>
2023-02-17 11:21:23 -0800

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
        </div>
        <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000] ease-in-out duration-500 ':'fixed left-[-100%]'}>
-       <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>React</h1>
+       <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>BoloForms</h1>
         <ul className='p-4 uppercase '>
         <li className='p-4  border-b border-gray-100'>Home</li>
         <li className='p-4 border-b border-gray-100'>Company</li>
